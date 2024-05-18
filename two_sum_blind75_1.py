c14762b74941e4b124288a0a78adfbc24ca7fad0
dangesid1999 <siddange1999@gmail.com>
2024-05-18 05:54:44 +0530

    modified with function and removed the traditional code

--- two_sum_blind75_1.py
+++ two_sum_blind75_1.py
@@ -2,12 +2,14 @@
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-arr = [2,7,11,15]
-target = 9
-n = len(arr)
-
-for i in range(0,len(arr)):
-    for j in range(0,n-1):
-        if arr[i] + arr[j] == target:
-            print(i,j)
+def two_sum(arr):
+    for i in range(0,len(arr)):
+        for j in range(0,len(arr)-1):
+            if arr[i] + arr[j] == target:
+                print(i,j)
         break
+
+if __name__ =="__main__":
+    arr = [2,7,11,15]
+    target = 9
+    two_sum(arr)
