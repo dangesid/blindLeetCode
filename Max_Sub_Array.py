69be516c7dfcf4187f8b5f86781432b2677ca18b
dangesid1999 <siddange1999@gmail.com>
2024-05-23 18:01:16 +0530

    Naive approach for Max_Sub_Array - Sliding Window Problem

--- Max_Sub_Array.py
+++ Max_Sub_Array.py
@@ -11,4 +11,30 @@
 # Output: 23
 # Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
 
-print("max sub array logic goes here ")
+
+# Naive or Beginner approach --> time complexity n cube (n^3)
+# find the sum of all the sub array and return the subarray with max sum
+
+def max_subarray_sum(nums):
+    if not nums:
+        return 0, []
+
+    max_sum = float("-inf")
+    max_subarray = []
+
+    for i in range(0,len(nums)):
+        for j in range(i,len(nums)):
+            curr_sum = 0
+            sub_arr = []
+            for k in range(i,j+1):
+                curr_sum += nums[k]
+                sub_arr.append(nums[k])
+            if curr_sum > max_sum:
+                max_sum = curr_sum
+                max_subarray = sub_arr
+    return max_sum, max_subarray
+
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+max_sum , max_subarray = max_subarray_sum(nums)
+print(max_sum)
+print(max_subarray)
