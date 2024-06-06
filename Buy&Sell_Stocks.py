915ab0eb354e3d326591468b9f95bbbf2c132aaf
dangesid1999 <siddange1999@gmail.com>
2024-06-06 12:52:36 +0530

    2 more problems

--- Buy&Sell_Stocks.py
+++ Buy&Sell_Stocks.py
@@ -7,23 +7,17 @@
 # Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
 # Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
-arr = [7,6,4,3,1]
-
-
 #iterate through each item and compare every element if its negative leave it and positive
 #then compare all positive and return the min
 
 # arr[i] = price of stock
 # i = day
+arr = [7,1,5,3,6,4]
+
 list_1 = []
 for i in range(0,len(arr)):
-    for j in range(i,len(arr)-1):
+    for j in range(i+1,len(arr)):
         res = arr[j]-arr[i]
-        print(arr[i],arr[j])
         list_1.append(res)
 print(list_1)
 print(max(list_1))
-
-
-min_profit = arr[0]
-max_profit = 0
