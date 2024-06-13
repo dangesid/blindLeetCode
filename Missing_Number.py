5f044f3d62059d0d591612026ddb9ab7f0b5faaa
dangesid1999 <siddange1999@gmail.com>
2024-06-13 22:53:58 +0530

    added Leetcode OOPS code

--- Missing_Number.py
+++ Missing_Number.py
@@ -7,15 +7,33 @@
 # Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range
 # since it does not appear in nums.
 
-nums = [9,6,4,2,3,5,7,0,1]
+nums = [9, 6, 4, 2, 3, 5, 7, 0, 1]
 # sum of first n natural number - sum of the numbers in the arr
 
 num = len(nums)
-print(int(num*(num+1)/2))
-sum_1 = int(num*(num+1)/2)
+print(int(num * (num + 1) / 2))
+sum_1 = int(num * (num + 1) / 2)
 
 sum_2 = sum(nums)
 print(sum_2)
 
 missing_num = sum_1 - sum_2
 print(missing_num)
+
+# ------------------------ LEETCODE OOPS WAY -----------------------------
+
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        num = len(nums)
+        sum_1 = int(num * (num + 1) / 2)
+        sum_2 = sum(nums)
+        return sum_1 - sum_2
+
+
+solution = Solution()
+nums = [3, 0, 1]
+missing = solution.missingNumber(nums)
+print(missing)
